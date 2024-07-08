6ba694b5c61b4e1db2819c593de93be5decf53f9
jiwonp7747 <166030508+jiwonp7747@users.noreply.github.com>
2024-07-09 00:06:41 +0900

    Add files via upload

--- service/api/src/main/java/org/delivery/api/config/jpa/JpaConfig.java
+++ service/api/src/main/java/org/delivery/api/config/jpa/JpaConfig.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration // 설정임을 알림
 @EntityScan(basePackages = "org.delivery.db") // 경로에 있는  스캔하겠다.
-@EnableJpaRepositories(basePackages = "org.delivery.db") // 하위에 있는 레포도 빈으로 등록
+@EnableJpaRepositories(basePackages = "org.delivery.db") //JpaRepository 인터페이스 스캔
 public class JpaConfig {
 
 }
