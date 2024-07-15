083dc2acc4595cc19417f31b8da8a0207578471b
jiwonp7747 <166030508+jiwonp7747@users.noreply.github.com>
2024-07-15 10:23:35 +0900

    JWT 인증 로직 추가

--- service/api/interceptor/AuthorizationInterceptor.java
+++ service/api/interceptor/AuthorizationInterceptor.java
@@ -4,16 +4,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.TokenErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.token.business.TokenBusiness;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class AuthorizationInterceptor implements HandlerInterceptor {
+
+    private final TokenBusiness tokenBusiness;
+
     @Override //사전 검증
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
@@ -29,7 +41,22 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        // TODO header 검증
-        return true;
+
+        var accessToken=request.getHeader("authorization-token"); // 토큰 가져와서
+        if(accessToken==null){ //없으면 예외처리
+            throw new ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND);
+        }
+
+        var userId=tokenBusiness.validationAccessToken(accessToken); // 있으면 id 가져오기
+
+        if(userId!=null){ // id가 있으면
+            // 한가지 요청에 대해 글로벌 하게 저장할 수 있는 스레드 로컬
+            var requestContext= Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
+            requestContext.setAttribute("userId", userId, RequestAttributes.SCOPE_REQUEST); //request단위로 저장
+            return true;
+        }
+
+        //없으면 예외
+        throw new ApiException(ErrorCode.BAD_REQEUST, "인증 실패");
     }
 }
